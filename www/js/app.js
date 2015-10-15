b2118cc4c27d546348cd0c112e891bee9c284f00
yoshimura <yoshimura@srp.co.jp>
2015-10-16 00:10:08 +0900

    init

--- www/js/app.js
+++ www/js/app.js
@@ -3,16 +3,27 @@
 angular.module('myApp', ['ionic','ngCordova','myApp.controllers'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+//  $ionicPlatform.ready(function() {
+//    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//    // for form inputs)
+//    if(window.cordova && window.cordova.plugins.Keyboard) {
+//      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//    }
+//    if(window.StatusBar) {
+//      StatusBar.styleDefault();
+//    }
+//  });
+
+//    document.addEventListener("deviceready", function() {
+//      if(window.cordova && window.cordova.plugins.Keyboard) {
+//      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//    }
+//    if(window.StatusBar) {
+//      StatusBar.styleDefault();
+//    }
+//      }, false);
+
+
 })
 ;
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,19 +1,30 @@
 "use strict";
 
 angular.module('myApp.controllers', [])
-.controller('mainCtrl',function($scope,$ionicPlatform){
+.controller('mainCtrl',function($scope,$ionicPlatform,$cordovaStatusbar,$cordovaDevice){
+	
 	var _this=this;
 //	_this.token = 'GO';
 //	$scope.token = 'WO';
-    $ionicPlatform.ready(function() {
+	  
+	$ionicPlatform.ready(function() {
 		 //_this.token = $cordovaDevice.getUUID();
- 		_this.token = 'HELLO';
+//		$cordovaStatusbar.overlaysWebView(true);
+//		  $cordovaStatusbar.show();
+// 		_this.token = 'HELLO';
+// 		$scope.$apply();
+		_this.token = $cordovaDevice.getUUID() + " OK!!";
  		$scope.$apply();
  	});
     
     
     document.addEventListener("deviceready", function() {
-    	_this.token = $cordovaDevice.getUUID();
+//    	$scope.$apply();
+//		$cordovaStatusbar.overlaysWebView(true);
+//		$cordovaStatusbar.show();
+//    	_this.token = "HH";
+//    	_this.token = $cordovaDevice.getPlatform();
+//    	_this.token = $cordovaDevice.getUUID();
     	$scope.$apply();
       }, false);
 })
