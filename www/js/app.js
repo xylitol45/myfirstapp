dec86904940abddd244455bbceb5d48ccae3403a
yoshimura <yoshimura@srp.co.jp>
2015-10-18 18:12:49 +0900

    init

--- www/js/app.js
+++ www/js/app.js
@@ -3,19 +3,12 @@
 angular.module('myApp', ['ionic','ngCordova','myApp.controllers'])
 
 .run(function($ionicPlatform, $cordovaStatusbar) {	
-
 	document.addEventListener("deviceready", function() {
       $cordovaStatusbar.overlaysWebView(true);
    }, false);
 
 // どうもうまく動作しない
 //  $ionicPlatform.ready(function() {
-//    if(window.cordova && window.cordova.plugins.Keyboard) {
-//      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-//    }
-//    if(window.StatusBar) {
-//      StatusBar.styleDefault();
-//    }
 //  });
 })
 ;
