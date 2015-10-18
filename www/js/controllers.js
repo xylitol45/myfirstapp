058dfdf30df6f86442a409c2c1e528ef87603641
yoshimura <yoshimura@srp.co.jp>
2015-10-18 18:13:58 +0900

    init

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,14 +3,6 @@
 angular.module('myApp.controllers', [])
 .controller('mainCtrl',function($scope,$ionicPlatform,$cordovaDevice){
 	var _this=this;
-
-// うまく動作しない
-//    $ionicPlatform.ready(function() {
-//    	_this.model = $cordovaDevice.getModel();
-// 		_this.model = 'HELLO';
-// 		$scope.$apply();
-// 	});
-
     document.addEventListener("deviceready", function() {
     	_this.model = $cordovaDevice.getModel();
     	$scope.$apply();
