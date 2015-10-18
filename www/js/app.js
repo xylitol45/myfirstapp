9f329556cfbb83f3fee630d9ade9184253a8d772
xylitol45 <xylitol45@gmail.com>
2015-10-18 15:37:23 +0900

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -2,14 +2,16 @@
 
 angular.module('myApp', ['ionic','ngCordova','myApp.controllers'])
 
-.run(function($ionicPlatform) {	
+.run(function($ionicPlatform, $cordovaStatusbar) {	
     document.addEventListener("deviceready", function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+//    if(window.cordova && window.cordova.plugins.Keyboard) {
+//      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//    }
+//    if(window.StatusBar) {
+//      StatusBar.styleDefault();
+//    }
+    $cordovaStatusbar.overlaysWebView(true);
+
    }, false);
 //   $cordovaStatusbar.overlaysWebView(true);
 
