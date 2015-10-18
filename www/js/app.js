eb4a6a4f038976df8e7658818ea6328f433d5551
xylitol45 <xylitol45@gmail.com>
2015-10-18 15:28:43 +0900

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,7 @@
 angular.module('myApp', ['ionic','ngCordova','myApp.controllers'])
 
 .run(function($ionicPlatform) {	
+   $cordovaStatusbar.overlaysWebView(true);
 //  $ionicPlatform.ready(function() {
 //    if(window.cordova && window.cordova.plugins.Keyboard) {
 //      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
