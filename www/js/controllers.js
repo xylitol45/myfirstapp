1990d2cab8ae5565ac7fbc1a096933756de7ac22
xylitol45 <xylitol45@gmail.com>
2015-10-18 15:10:42 +0900

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,18 +3,17 @@
 angular.module('myApp.controllers', [])
 .controller('mainCtrl',function($scope,$ionicPlatform,$cordovaDevice){
 	var _this=this;
-//	_this.token = 'GO';
-//	$scope.token = 'WO';
-    $ionicPlatform.ready(function() {
-		 //_this.token = $cordovaDevice.getUUID();
- 		_this.model = 'HELLO';
- 		$scope.$apply();
- 	});
-    
-    
+
+// うまく動作しない
+//    $ionicPlatform.ready(function() {
+//    	_this.model = $cordovaDevice.getModel();
+// 		_this.model = 'HELLO';
+// 		$scope.$apply();
+// 	});
+
     document.addEventListener("deviceready", function() {
-    	// _this.model = $cordovaDevice.getModel();
-    	// $scope.$apply();
+    	_this.model = $cordovaDevice.getModel();
+    	$scope.$apply();
       }, false);
 })
 ;
