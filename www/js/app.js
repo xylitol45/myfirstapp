453b7aa1c69118b65610092e1e7112f78c5e5946
yoshimura <yoshimura@srp.co.jp>
2015-10-18 19:17:15 +0900

    init

--- www/js/app.js
+++ www/js/app.js
@@ -4,12 +4,16 @@ angular.module('myApp', ['ionic','ngCordova','myApp.controllers'])
 
 .run(function($ionicPlatform, $cordovaStatusbar) {	
 	document.addEventListener("deviceready", function() {
-      $cordovaStatusbar.overlaysWebView(true);
+      //$cordovaStatusbar.overlaysWebView(true);
    }, false);
 
 // どうもうまく動作しない
 //  $ionicPlatform.ready(function() {
 //  });
+	ionic.Platform.ready(function(){
+	    $cordovaStatusbar.overlaysWebView(true);
+	 });
+
 })
 ;
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,8 +4,13 @@ angular.module('myApp.controllers', [])
 .controller('mainCtrl',function($scope,$cordovaDevice){
 	var _this=this;
     document.addEventListener("deviceready", function() {
+//    	_this.model = $cordovaDevice.getModel();
+//    	$scope.$apply();
+      }, false);
+	ionic.Platform.ready(function(){
     	_this.model = $cordovaDevice.getModel();
     	$scope.$apply();
-      }, false);
+	 });
+
 })
 ;
