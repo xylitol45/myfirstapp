5fba43955f9dbc6e175cd8afa4bec50ba64ae1fe
yoshimura <yoshimura@srp.co.jp>
2015-10-18 14:42:57 +0900

    init

---
+++ www/js/controllers.js
@@ -0,0 +1,20 @@
+"use strict";
+
+angular.module('myApp.controllers', [])
+.controller('mainCtrl',function($scope,$ionicPlatform){
+	var _this=this;
+//	_this.token = 'GO';
+//	$scope.token = 'WO';
+    $ionicPlatform.ready(function() {
+		 //_this.token = $cordovaDevice.getUUID();
+ 		_this.token = 'HELLO';
+ 		$scope.$apply();
+ 	});
+    
+    
+    document.addEventListener("deviceready", function() {
+    	_this.token = $cordovaDevice.getUUID();
+    	$scope.$apply();
+      }, false);
+})
+;
